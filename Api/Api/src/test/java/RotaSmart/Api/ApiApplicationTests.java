3fdb09b0e387af887c6cb8df161088f7c6d065e9
Sergio_jrsilva <juniosergio034@gmail.com>
2025-07-21 20:19:04 -0300

    criação do projeto

---
+++ Api/Api/src/test/java/RotaSmart/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package RotaSmart.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
